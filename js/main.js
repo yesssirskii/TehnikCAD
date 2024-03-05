5d7e2a0e9cf7cad6e645282a7433f8a2c5bc68a1
yesssirskii <ivanmatejcic1@gmail.com>
2024-03-05 20:41:51 +0100

    Added automatic year change, fixed back to top button, added careers image

--- js/main.js
+++ js/main.js
@@ -55,52 +55,19 @@
     
     
     // Back to top button
-    /*$(window).scroll(function () {
+    $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
             $('.back-to-top').fadeIn('slow');
         } else {
             $('.back-to-top').fadeOut('slow');
         }
     });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });*/
-
-    // Header carousel
-    $(".header-carousel").owlCarousel({
-        autoplay: false,
-        smartSpeed: 1500,
-        items: 1,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ]
-    });
-
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: false,
-        smartSpeed: 1000,
-        center: true,
-        dots: true,
-        loop: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
     
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 100, 'easeInOutExpo');
+
+        return false;
+    });
+
 })(jQuery);
 
